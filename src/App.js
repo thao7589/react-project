d73336c3fefc45de1b8c37dc0b77b7e25d542b7d
thaole <thao@codeforcevina.com>
2018-06-21 18:59:03 +0700

    change name

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+    constructor(props) {
+      super(props);
+      this.state = {
+          name: ''
+      };
+      this.change = (ev) => {
+          console.log(ev)
+          this.setState({
+                  name: ev.target.value
+              }
+          )
+        }
+    }
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Hello!
-        </p>
-      </div>
-    );
-  }
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+
+            <p className="App-intro">
+              Hello {this.state.name}!
+            </p>
+              <input type="text" onChange={this.change}></input>
+          </div>
+        );
+    }
 }
 
 export default App;
